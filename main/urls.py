6cb526274eff0672763580df85f87efc31a2a4cf
omosh-notlim <omondimilton43@gmail.com>
2024-07-02 11:21:05 +0300

    User & post management django-app

---
+++ main/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('home', views.home, name='home'),
+    path('create-post', views.create_post, name='create_post'),
+    path('sign-up', views.sign_up, name='sign_up'),
+    path('logout/', views.logout_user, name='logout'),
+]
